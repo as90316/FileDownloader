aa858e14735fefe52581ea17c594ac3a8f8495de
ASHUTOSH SHARMA <ashutosh5.sharma@gmail.com>
2019-04-17 00:22:04 +0530

    FileDownloader
    Simple File Downloader

---
+++ FileDownloader/FileDownloader.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+class FileDownloaderClass
+{
+    private string url;
+    private string targetFile;
+
+    public string URL
+    {
+        //getter and setter
+        get { return url; }
+        set {
+            if (!string.IsNullOrEmpty(value))
+            {
+                url = value;
+            } else
+            {
+                throw new System.ArgumentNullException();
+            }
+          }
+    }
+    public string DEST
+    { 
+        get { return targetFile; }
+        set {
+            if (!string.IsNullOrEmpty(value))
+            { targetFile = @"C:\temp\" + value; }
+            else { targetFile = @"C:\temp\a.dat"; }
+        }
+    }
+
+    
+    public void DownloadAndSave()
+    {
+        WebClient wc = new WebClient();
+       wc.DownloadFile(new System.Uri( this.URL),this.DEST);
+    }
+    
+}
---
+++ FileDownloader/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello");
+                FileDownloaderClass f = new FileDownloaderClass();
+                Console.WriteLine("Enter the complete URL: e.g. http://www.africau.edu/images/default/sample.pdf");
+                f.URL = Console.ReadLine();
+                Console.WriteLine("Enter the file to be saved as? e.g. mypdf.pdf");
+                f.DEST = Console.ReadLine();
+                f.DownloadAndSave();
+                Console.WriteLine("Download completed at " + f.DEST + "   ...Good Bye");
+            }
+            catch (System.ArgumentNullException e) {
+                Console.WriteLine("URL " + e.Message);
+                return;
+            }
+        }
+    }
+}
